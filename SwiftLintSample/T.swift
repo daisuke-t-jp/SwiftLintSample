71bf5e5f1270667e0ddb41d631fb397cd600061f
daisuke-t-jp <daisuke.t.jp@gmail.com>
2019-08-13 12:04:09 +0900

    Add Too short name Class.

---
+++ SwiftLintSample/T.swift
@@ -0,0 +1,14 @@
+//
+//  T.swift
+//  SwiftLintSample
+//
+//  Created by Daisuke T on 2019/08/13.
+//  Copyright © 2019 DaisukeT. All rights reserved.
+//
+
+import Foundation
+
+// Too short name Class.
+class T {
+  
+}
