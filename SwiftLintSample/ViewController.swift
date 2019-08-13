f970f718ca77debf778e25afb9ec1275609e3d6b
daisuke-t-jp <daisuke.t.jp@gmail.com>
2019-08-13 09:08:37 +0900

    Add Project

---
+++ SwiftLintSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftLintSample
+//
+//  Created by Daisuke T on 2019/08/13.
+//  Copyright © 2019 DaisukeT. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
---
+++ SwiftLintSample/lowerCamelClass.swift
@@ -0,0 +1,13 @@
+//
+//  lowerCamelClass.swift
+//  SwiftLintSample
+//
+//  Created by Daisuke T on 2019/08/13.
+//  Copyright © 2019 DaisukeT. All rights reserved.
+//
+
+import Foundation
+
+class lowerCamelClass {
+  let v = 0
+}
